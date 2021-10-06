956c093839832a5d75a3ae31129df2aac58e2345
Artem <64410376+Artiss90@users.noreply.github.com>
2021-10-06 21:31:11 +0300

    корректное отображение на мобильном

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import AppBar from 'components/AppBar/AppBar';
-import style from './App.module.scss'
+import style from './App.module.css'
 
 function App() {
-  return <div className={style.container}>
+  const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight() // ? динамически определяем высоту экрана и запишем её в css переменную
+
+  return <div className={style.containerApp}>
     <AppBar/>
   </div>;
 }
