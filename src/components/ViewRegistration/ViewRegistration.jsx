d1a4d46634dc714528681929fc480c36cfd7dcdc
Artem <64410376+Artiss90@users.noreply.github.com>
2021-11-17 20:10:52 +0200

    edit link for prod

--- src/components/ViewRegistration/ViewRegistration.jsx
+++ src/components/ViewRegistration/ViewRegistration.jsx
@@ -131,7 +131,7 @@ const handleSubmitRegister = (e) => {
           return result
           }
           console.error('в ответе пришел не статус 200');})
-          .then(result => axios.post(`https://api.chatbullet.com/api/v1/send/a72e2cfc7a7acc4989f308f263f3ee12`, {token: result?.data?.token, user: result?.data?.data, id: role}))
+          .then(result => axios.post(`https://api.chatbullet.com/api/v1/send/a72e2cfc7a7acc4989f308f263f3ee12/1000345`, {token: result?.data?.token, user: result?.data?.data, id: role}))
           .then(result=>{
             setIsSuccessRegistrated(true)
           })
